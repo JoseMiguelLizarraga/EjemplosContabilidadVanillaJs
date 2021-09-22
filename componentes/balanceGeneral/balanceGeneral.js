84137fedfd4e1159f695b0eeb66971f9b6238f39
jmlizarraga <jlizarraga@chilesystems.com>
2021-09-22 10:11:18 -0300

    Simplificar tabla asientos

--- componentes/balanceGeneral/balanceGeneral.js
+++ componentes/balanceGeneral/balanceGeneral.js
@@ -141,86 +141,6 @@ export class BalanceGeneral extends CargaVista
         let sumaDebe = 0;
         let sumaHaber = 0;
 
-        let cadenaAsientosContables = `
-        <table border="1">
-            <thead style="background-color: #55514d; color: #eceaea;">
-                <tr>
-                    <th> Fecha </th>
-                    <th> Título </th>
-                    <th style='width: 330px;'> Descripción General </th>
-                    <th> Descripción Específica </th>
-                    <th> Cuenta </th>
-                    <th> Debe </th>
-                    <th> Haber </th>
-                </tr>
-            </thead>
-            <tbody>
-
-            ${libro.asientosContables.map((asiento, cont) => 
-            { 
-                let estilosFilaAsiento = `background-color: ${cont % 2 == 0 ? "#fff4ee" : "#ced0c6"};`;
-
-                return `
-                <tr style="${estilosFilaAsiento}">
-                    <td> ${asiento.fecha} </td>
-                    <td> <b> ${asiento.nombre} </b> </td>
-                    <td> ${asiento.descripcion} </td>
-                    <td> </td>
-                    <td> </td>
-                    <td> </td>
-                    <td> </td>
-                </tr>
-
-                ${asiento.detallesDebe.map(detalleDebe => 
-                {
-                    sumaDebe += detalleDebe.cantidad;
-                    return `
-                    <tr style="${estilosFilaAsiento}">
-                        <td> </td>
-                        <td> </td>
-                        <td> </td>
-                        <td> ${detalleDebe.descripcionCarga} </td>
-                        <td> ${detalleDebe.nombreCuenta} </td>
-                        <td> ${formatearNumero(detalleDebe.cantidad)} </td>
-                        <td> </td>
-                    </tr>
-                    `
-                }).join("")}
-
-                ${asiento.detallesHaber.map(detalleHaber => 
-                {
-                    sumaHaber += detalleHaber.cantidad;
-                    return `
-                    <tr style="${estilosFilaAsiento}">
-                        <td> </td>
-                        <td> </td>
-                        <td> </td>
-                        <td> ${detalleHaber.descripcionAbono} </td>
-                        <td> ${detalleHaber.nombreCuenta} </td>
-                        <td> </td>
-                        <td> ${formatearNumero(detalleHaber.cantidad)} </td>
-                    </tr>
-                    `
-                }).join("")}
-                ` 
-            }).join("")} 
-            </tbody>
-            <tfoot style="background-color: #c3dde6;">
-                <tr>
-                    <td colspan="5"> </td>
-                    <td> <b> Suma Debe </b> </td>
-                    <td> <b> Suma Haber </b> </td>
-                </tr>
-                <tr>
-                    <td colspan="5"> </td>
-                    <td> ${formatearNumero(sumaDebe)} </td>
-                    <td> ${formatearNumero(sumaHaber)} </td>
-                </tr>
-            </tfoot>
-        </table>
-
-        `;
-
         let textoHtml = `
         <br/><br/>
         <h3> Balance General </h3>
@@ -228,7 +148,7 @@ export class BalanceGeneral extends CargaVista
     
         <br/><br/>
         <h3> Asientos contables utilizados </h3>
-        ${cadenaAsientosContables}
+        ${libro.obtenerTablaAsientosLibro()}
     
         <br/><br/>
         `;
--- modelo/libro.js
+++ modelo/libro.js
@@ -1,3 +1,4 @@
+import { formatearNumero } from "../funciones/funcionesGenericas.js";
 
 
 
@@ -6,5 +7,95 @@ export class Libro
     constructor() {
         this.asientosContables = [];
     }
-    agregarAsiento(asiento) { this.asientosContables.push(asiento); }
+    
+    agregarAsiento(asiento) { 
+        this.asientosContables.push(asiento); 
+    }
+
+    obtenerTablaAsientosLibro()
+    {
+        let sumaDebe = 0;
+        let sumaHaber = 0;
+
+        let cadena = `
+        <table border="1">
+            <thead style="background-color: #55514d; color: #eceaea;">
+                <tr>
+                    <th> Fecha </th>
+                    <th> Título </th>
+                    <th style='width: 330px;'> Descripción General </th>
+                    <th> Descripción Específica </th>
+                    <th> Cuenta </th>
+                    <th> Debe </th>
+                    <th> Haber </th>
+                </tr>
+            </thead>
+            <tbody>
+
+            ${this.asientosContables.map((asiento, cont) => 
+            { 
+                let estilosFilaAsiento = `background-color: ${cont % 2 == 0 ? "#fff4ee" : "#ced0c6"};`;
+
+                return `
+                <tr style="${estilosFilaAsiento}">
+                    <td> ${asiento.fecha} </td>
+                    <td> <b> ${asiento.nombre} </b> </td>
+                    <td> ${asiento.descripcion} </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                </tr>
+
+                ${asiento.detallesDebe.map(detalleDebe => 
+                {
+                    sumaDebe += detalleDebe.cantidad;
+                    return `
+                    <tr style="${estilosFilaAsiento}">
+                        <td> </td>
+                        <td> </td>
+                        <td> </td>
+                        <td> ${detalleDebe.descripcionCarga} </td>
+                        <td> ${detalleDebe.nombreCuenta} </td>
+                        <td> ${formatearNumero(detalleDebe.cantidad)} </td>
+                        <td> </td>
+                    </tr>
+                    `
+                }).join("")}
+
+                ${asiento.detallesHaber.map(detalleHaber => 
+                {
+                    sumaHaber += detalleHaber.cantidad;
+                    return `
+                    <tr style="${estilosFilaAsiento}">
+                        <td> </td>
+                        <td> </td>
+                        <td> </td>
+                        <td> ${detalleHaber.descripcionAbono} </td>
+                        <td> ${detalleHaber.nombreCuenta} </td>
+                        <td> </td>
+                        <td> ${formatearNumero(detalleHaber.cantidad)} </td>
+                    </tr>
+                    `
+                }).join("")}
+                ` 
+            }).join("")} 
+            </tbody>
+            <tfoot style="background-color: #c3dde6;">
+                <tr>
+                    <td colspan="5"> </td>
+                    <td> <b> Suma Debe </b> </td>
+                    <td> <b> Suma Haber </b> </td>
+                </tr>
+                <tr>
+                    <td colspan="5"> </td>
+                    <td> ${formatearNumero(sumaDebe)} </td>
+                    <td> ${formatearNumero(sumaHaber)} </td>
+                </tr>
+            </tfoot>
+        </table>
+        `;
+
+        return cadena;
+    }
 }
